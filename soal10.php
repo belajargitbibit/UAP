9a3026b552918f5bca241073fe637ffb58542529
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2023-06-26 20:30:55 +0700

    soal UAP selesai

---
+++ soal10.php
@@ -0,0 +1,6 @@
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[1] . " , " . $cars[2] . " and " . $cars[0];
+// apabila ; dan tanda " diletakkan setelah script penutup php, maka script akan error. 
+//Script ini saya betulkan hingga muncul keluaran hasil yang benar
+?>
